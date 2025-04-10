be329e534248febdd93f900267532c51f063b9f7
Ilya Chibisov <i.chibisov@gmail.com>
2025-04-10 15:17:54 +0700

    замена функции на Remove

--- vfs/vfscache/item.go
+++ vfs/vfscache/item.go
@@ -613,7 +613,7 @@ func (item *Item) _store(ctx context.Context, storeFn StoreFn) (err error) {
 	            if err != nil {
 		            fs.Errorf(item.name, "vfs cache: failed to write metadata file: %v", err)
 	            }
-				removeErr := item.c.RemoveVirtual(item.name)
+				removeErr := item.c.Remove(item.name)
 		        if removeErr != nil {
 			        fs.Errorf(item.name, "vfs cache: failed to remove from virtual FS: %v", removeErr)
 		        }
